250cc88c7603b8c2be77731bd3e85d85fe0a65b2
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-11 22:15:12 +0530

    finalized email module

---
+++ src/modules/EmailModule/UpdateEmailModule/index.jsx
@@ -0,0 +1,42 @@
+import NotFound from '@/components/NotFound';
+import { ErpLayout } from '@/layout';
+import UpdateItem from '@/modules/ErpPanelModule/UpdateItem';
+import EmailForm from './components/EmailForm';
+import PageLoader from '@/components/PageLoader';
+import { erp } from '@/redux/erp/actions';
+import { selectReadItem } from '@/redux/erp/selector';
+import { useLayoutEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+export default function UpdateEmailModule({ config }) {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useLayoutEffect(() => {
+    dispatch(erp.read({ entity: config.entity, id }));
+  }, [id]);
+
+  const { result: currentResult, isSuccess, isLoading = true } = useSelector(selectReadItem);
+  useLayoutEffect(() => {
+    if (currentResult) {
+      dispatch(erp.currentAction({ actionType: 'update', data: currentResult }));
+    }
+  }, [currentResult]);
+  if (isLoading) {
+    return (
+      <ErpLayout>
+        <PageLoader />
+      </ErpLayout>
+    );
+  } else
+    return (
+      <ErpLayout>
+        {isSuccess ? (
+          <UpdateItem config={config} UpdateForm={EmailForm} />
+        ) : (
+          <NotFound entity={config.entity} />
+        )}
+      </ErpLayout>
+    );
+}
