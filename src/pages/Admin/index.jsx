f896e6faa99becfa3f5156c3692e39736bda9bda
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-11 22:55:31 +0530

    started making pages

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,2 +1,59 @@
-import React from "react";
-import useLanguage from "@/locale/useLanguage";
+import React from 'react';
+import useLanguage from '@/locale/useLanguage';
+import AdminCrudModule from '@/modules/AdminCrudModule';
+import AdminForm from '@/forms/AdminForm';
+
+export default function Admin() {
+  const translate = useLanguage();
+  const entity = 'admin';
+  const searchConfig = {
+    displayLabels: ['name', 'surname'],
+    searchFields: 'email,name,surname',
+    outputValue: '_id',
+  };
+
+  const entityDisplayLabels = ['email'];
+
+  const readColumns = [
+    { title: translate('fisrt Name'), dataIndex: 'name' },
+    { title: translate('last Name'), dataIndex: 'surname' },
+    { title: translate('Email'), dataIndex: 'email' },
+    { title: translate('role'), dataIndex: 'role' },
+  ];
+
+  const dataTableColumns = [
+    { title: translate('first name'), dataIndex: 'name' },
+    { title: translate('last name'), dataIndex: 'surname' },
+    { title: translate('Email'), dataIndex: 'email' },
+    { title: translate('role'), dataIndex: 'role' },
+  ];
+
+  const Labels = {
+    PANEL_TITLE: translate('admin'),
+    DATATABLE_TITLE: translate('amdin_ist'),
+    ADD_NEW_ENTITY: translate('add_new-admin'),
+    ENTITY_NAME: translate('admin'),
+    CREATE_ENTITY: translate('save'),
+    UPDATE_ENTITY: translate('update'),
+    RECORD_ENTITY: translate('record_payment'),
+  };
+
+  const configPage = {
+    entity,
+    ...Labels,
+  };
+  const config = {
+    ...configPage,
+    readColumns,
+    dataTableColumns,
+    searchConfig,
+    entityDisplayLabels,
+  };
+  return (
+    <AdminCrudModule
+      CreateForm={<AdminForm />}
+      updateForm={<AdminForm isUpdateForm={true} />}
+      config={config}
+    />
+  );
+}
