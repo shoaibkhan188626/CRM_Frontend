39c9ed85d7b0dea569ecaa32fd84ad0dc3f95d2b
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-12 13:38:37 +0530

    payment update page added

---
+++ src/pages/Payment/PaymentUpdate.jsx
@@ -0,0 +1,22 @@
+import useLanguage from '@/locale/useLanguage';
+import UpdatePaymentModule from '@/modules/PaymentModule/UpdatePaymentModule';
+
+export default function PaymentModule() {
+  const translate = useLanguage();
+  const entity = 'payment';
+
+  const Labels = {
+    PANEL_TITLE: translate('payment'),
+    DATATABLE_TITLE: translate('payment_list'),
+    ADD_NEW_ENTITY: translate('add_new_payment'),
+    ENTITY_NAME: translate('payment'),
+    CREATE_ENTITY: translate('save'),
+    UPDATE_ENTITY: translate('update'),
+  };
+
+  const configPage = {
+    entity,
+    ...Labels,
+  };
+  return <UpdatePaymentModule config={configPage} />;
+}
