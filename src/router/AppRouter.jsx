ed0130b1c01c74058505aa4895073ab813472819
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 18:19:51 -0500

    Auto-commit: Changes made on save

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,8 +3,10 @@ import { useRoutes } from 'react-router-dom';
 import PublicRoute from './PublicRoute';
 
 const Logout = lazy(() => import('@/pages/Logout.jsx'));
-const NotFound = lazy(()=>import ('@/pages/NotFound.JSX'))
-const Dashboard = lazy(()=>import('@/pages/Dashboard.jsx'))
-const Customer= lazy(()=>import('@/pages/Customer'))
-const Inventory=lazy(()=>import('@/pages/Inventory'))
-const 
+const NotFound = lazy(() => import('@/pages/NotFound.JSX'));
+const Dashboard = lazy(() => import('@/pages/Dashboard.jsx'));
+const Customer = lazy(() => import('@/pages/Customer'));
+const Inventory = lazy(() => import('@/pages/Inventory'));
+const Order = lazy(() => import('@/pages/Order'));
+const Invoice = lazy(() => import('@/pages/Invoice'));
+const InvoiceCreate=lazy(()=>import('@/pages/Invo'))
