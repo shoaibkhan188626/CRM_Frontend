725fd18964e06992f131afe973f746136d01e8de
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 18:15:47 -0500

    Auto-commit: Changes made on save

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,3 +1,6 @@
 import { lazy } from 'react';
 import { useRoutes } from 'react-router-dom';
 import PublicRoute from './PublicRoute';
+
+const Logout = lazy(() => import('@/pages/Logout.jsx'));
+const NotFound = lazy(()=>import )
