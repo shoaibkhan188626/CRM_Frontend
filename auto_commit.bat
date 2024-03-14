fbacde4945e12ad2459a0771f87cbe1d92f45d08
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 16:14:02 -0500

    added batch script to auto push the code on save

--- auto_commit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Add all changes
-git add .
-:: Commit changes with a message
-git commit -m "Auto-commit: Changes made on save"
-:: Push changes to the remote repository
-git push origin main
