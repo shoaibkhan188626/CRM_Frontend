70a90c56acf5159ac20240e3636957a51284c022
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 17:11:07 -0500

    Auto-commit: Changes made on save

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -34,15 +34,25 @@ const RegisterPage = () => {
             <div className="space50"></div>
           </Col>
           <Title level={1}>{translate('Sign up')}</Title>
-          <Divider/>
-          <div className='site-layout-content'>
+          <Divider />
+          <div className="site-layout-content">
             <Form
-            name='sginup'
-            className='login-form'
-            initialValues={{
-              
-            }}
-            ></Form>
+              name="sginup"
+              className="login-form"
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+            >
+              <RegisterForm />
+              <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button" size="large">
+                  {translate('Register')}
+                </Button>
+                {translate('Or')}
+                <a href="/login">{translate('already have account Login ?')}</a>
+              </Form.Item>
+            </Form>
           </div>
         </Content>
       </AuthLayout>
