48164fc83807d7f2d808828c117b8d0bf94fd887
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 17:31:18 -0500

    Auto-commit: Changes made on save

---
+++ src/pages/Invoice/InvoiceUpdate.jsx
@@ -0,0 +1,15 @@
+import useLanguage from "@/locale/useLanguage";
+import UpdateInvoiceModule from "@/modules/InvoiceModule/UpdateInvoiceModule";
+
+export default function InvoiceUpdate(){
+    const entity='invoice';
+    const translate=useLanguage();
+    const Labels={
+        PANEL_TITLE,
+        DATATABLE_TITLE,
+        ADD_NEW_ENTITY,
+        ENTITY_NAME,
+        CREATE_ENTITY,
+        
+    }
+}
