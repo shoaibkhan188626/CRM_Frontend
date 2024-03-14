d31ca41459370f41858588c42aea5c19843ed772
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 17:33:20 -0500

    Auto-commit: Changes made on save

--- src/pages/Invoice/InvoiceUpdate.jsx
+++ src/pages/Invoice/InvoiceUpdate.jsx
@@ -1,15 +1,21 @@
-import useLanguage from "@/locale/useLanguage";
-import UpdateInvoiceModule from "@/modules/InvoiceModule/UpdateInvoiceModule";
+import useLanguage from '@/locale/useLanguage';
+import UpdateInvoiceModule from '@/modules/InvoiceModule/UpdateInvoiceModule';
 
-export default function InvoiceUpdate(){
-    const entity='invoice';
-    const translate=useLanguage();
-    const Labels={
-        PANEL_TITLE,
-        DATATABLE_TITLE,
-        ADD_NEW_ENTITY,
-        ENTITY_NAME,
-        CREATE_ENTITY,
-        
-    }
+export default function InvoiceUpdate() {
+  const entity = 'invoice';
+  const translate = useLanguage();
+  const Labels = {
+    PANEL_TITLE: translate('invoice'),
+    DATATABLE_TITLE: translate('invoice_list'),
+    ADD_NEW_ENTITY: translate('add_new_invoice'),
+    ENTITY_NAME: translate('invoice'),
+    CREATE_ENTITY: translate('save'),
+    UPDATE_ENTITY: translate('update'),
+    RECORD_ENITY: translate('record_payment'),
+  };
+  const configPage = {
+    entity,
+    ...Labels,
+  };
+  return <UpdateInvoiceModule config={configPage} />;
 }
