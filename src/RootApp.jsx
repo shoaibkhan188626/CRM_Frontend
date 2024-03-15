708307a320e560efd91f7e75a936b72c3875586e
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 19:36:55 -0500

    Auto-commit: Changes made on save

--- src/RootApp.jsx
+++ src/RootApp.jsx
@@ -1,2 +1,20 @@
 import './style/app.css';
-import { Suspense,La } from 'react';
+import { Suspense, lazy } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
+import PageLoader from './components/PageLoader';
+
+const IdurarOs = lazy(() => import('./apps/IdurarOs'));
+
+export default function RoutApp() {
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <Suspense fallback={<PageLoader />}>
+          <IdurarOs />
+        </Suspense>
+      </Provider>
+    </BrowserRouter>
+  );
+}
