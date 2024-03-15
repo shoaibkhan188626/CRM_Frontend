97c58ff91c45f3860974eecab59eb261655f4a71
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 19:08:36 -0500

    Auto-commit: Changes made on save

--- src/apps/IdurarOs.jsx
+++ src/apps/IdurarOs.jsx
@@ -1,6 +1,22 @@
-import {lazy,Suspense} from 'react'
-import {useSelector} from 'react-redux'
-import {selectAuth} from '@/redux/auth/selector'
-import { AppContextProvider } from '@/context/appContext'
-import PageLoader from '@/components/PageLoader'
-import Localization from '@/locale/Localization'
+import { lazy, Suspense } from 'react';
+import { useSelector } from 'react-redux';
+import { selectAuth } from '@/redux/auth/selector';
+import { AppContextProvider } from '@/context/appContext';
+import PageLoader from '@/components/PageLoader';
+import Localization from '@/locale/Localization';
+import AuthRouter from '@/router/AuthRouter';
+
+const ErpApp = lazy(() => import('./ErpApp'));
+
+export default function IdurarOs() {
+  const { isLoggedIn } = useSelector(selectAuth);
+
+  if (!isLoggedIn)
+    return (
+      <Localization>
+        <Suspense fallback={<PageLoader />}>
+          <AuthRouter />
+        </Suspense>
+      </Localization>
+    );
+}
