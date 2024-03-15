5f3fe4a127e6cd3b5a7fbd8945f33351c9921282
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 19:02:30 -0500

    Auto-commit: Changes made on save

---
+++ src/router/AuthRouter.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from '@/pages/Login';
+import NotFound from '@/components/NotFound';
+import Register from '@/pages/Register';
+
+export default function AuthRouter(){
+    return
+    <Routes>
+        <
+    </Routes>
+}
