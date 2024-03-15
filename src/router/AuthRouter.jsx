904834d232d3bfc8d693a59236f0f93705ab2cff
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-14 19:04:32 -0500

    Auto-commit: Changes made on save

--- src/router/AuthRouter.jsx
+++ src/router/AuthRouter.jsx
@@ -3,9 +3,14 @@ import Login from '@/pages/Login';
 import NotFound from '@/components/NotFound';
 import Register from '@/pages/Register';
 
-export default function AuthRouter(){
-    return
+export default function AuthRouter() {
+  return (
     <Routes>
-        <
+      <Route element={<Login />} path="/" />
+      <Route element={<Login />} path="/login" />
+      <Route element={<Login />} path="/logout" />
+      <Route element={<Register />} path="/register" />
+      <Route path="*" element={<NotFound />} />
     </Routes>
+  );
 }
